c6963b625d5d96ba5693f404885fad821e3e564e
Kent Mogler <kentmogler@Kents-iMac.local>
2014-11-19 21:50:42 -0600

    Initial Commit

---
+++ Farm Application/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Farm Application
+//
+//  Created by Kent Mogler on 11/19/14.
+//  Copyright (c) 2014 Kent Mogler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
